41bd3991e5dc4f370e3834de740bac9a37912ce2
Lukas Stevens <lukas.steve@web.de>
2017-11-18 17:55:16 +0100

    Add to sentences utility function

--- python-backend/nts_backend/tokenizer.py
+++ python-backend/nts_backend/tokenizer.py
@@ -3,6 +3,7 @@ from .config import config
 import requests
 import json
 
+
 class Tokenizer:
     API_URL = 'https://westus.api.cognitive.microsoft.com/linguistics/v1.0/analyze'
 
@@ -20,5 +21,16 @@ class Tokenizer:
             'Ocp-Apim-Subscription-Key': self.api_key
         }
 
-        req = requests.post(Tokenizer.API_URL, data=json.dumps(payload), headers=headers)
+        req = requests.post(
+                Tokenizer.API_URL, data=json.dumps(payload), headers=headers)
         return json.loads(req.content)[0]['result']
+
+
+def tokens_to_sentences(tokenization):
+    sentences = []
+    for sentence_token in tokenization:
+        print(sentence_token)
+        word_list = [token['NormalizedToken'] for token in sentence_token['Tokens']]
+        sentence = ' '.join(word_list)
+        sentences.append(sentence)
+    return sentences
