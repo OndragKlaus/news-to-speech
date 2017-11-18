a8a7b1c598a3b7cbe4c50d308255b6e9a6717884
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-11-18 09:11:13 +0100

    added author and category to db

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,12 +1,23 @@
+DROP TABLE Provider;
+DROP TABLE Article;
+
+
 CREATE TABLE IF NOT EXISTS Provider (
   provider_id BIGSERIAL PRIMARY KEY,
+  name        TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Category (
+  category_id BIGSERIAL PRIMARY KEY,
   name        TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Article (
   article_id      BIGSERIAL PRIMARY KEY,
   provider_id     BIGSERIAL REFERENCES Provider (provider_id),
+  category_id     BIGSERIAL REFERENCES Category (category_id),
   guid            TEXT NOT NULL UNIQUE,
+  author          TEXT,
   title           TEXT NOT NULL,
   body            TEXT NOT NULL,
   summary         TEXT,
