22943ce016443eaafc2c9f1e6e53bfb507e5e2c6
Amar Šaljić <amarsaljic@w211-6k-v4.eduroam.dynamic.rbg.tum.de>
2017-11-18 22:29:48 +0100

    ListView almost ready

--- android-frontend/app/src/main/java/com/example/amarsaljic/newstospeech/MainActivity.java
+++ android-frontend/app/src/main/java/com/example/amarsaljic/newstospeech/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity implements ISpeechRecognitio
     TextView checkIntent;
     MenuItem startSpeechRecognitionItem;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
