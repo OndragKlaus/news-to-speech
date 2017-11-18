6c79ffa1097e785ea82a7ffc3576eec768020111
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-11-18 19:38:27 +0100

    added PlayActivity

---
+++ android-frontend/app/src/main/java/com/example/amarsaljic/newstospeech/PlayActivity.java
@@ -0,0 +1,34 @@
+package com.example.amarsaljic.newstospeech;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        final MediaPlayer example = MediaPlayer.create(PlayActivity.this, R.raw.a1);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                if (example.isPlaying()) {
+                    example.stop();
+                } else {
+                    example.start();
+                }
+            }
+        });
+    }
+
+}
