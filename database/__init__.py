97135fb3215aa9c678d4d3c764ffa0c4c2979798
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-18 11:48:25 +0100

    add more functions to database api

--- database/__init__.py
+++ database/__init__.py
@@ -48,3 +48,51 @@ def get_provider_id(name, create=False):
     if row is None:
         raise ValueError('provider {!r} does not exist'.format(name))
     return row[0]
+
+
+def get_category_id(name, create=False):
+    cur = conn.cursor()
+    if create:
+        cur.execute("""
+            INSERT INTO Category (name) SELECT %s
+            WHERE NOT EXISTS (SELECT 1 FROM Category WHERE name ILIKE %s)
+        """, (name, name))
+        conn.commit()
+    cur.execute("SELECT category_id FROM Category WHERE name ILIKE %s", (name,))
+    row = cur.fetchone()
+    if row is None:
+        raise ValueError('category {!r} does not exist'.format(name))
+    return row[0]
+
+
+def has_article_with_guid(guid):
+    cur = conn.cursor()
+    cur.execute("SELECT 1 FROM Article WHERE guid = %s", (guid,))
+    return bool(cur.fetchone())
+
+
+def delete_article_by_guid(guid, notexist_ok=False):
+    cur = conn.cursor()
+    # XXX get the number of rows that were deleted? Instead of this:
+    if not notexist_ok and not has_article_with_guid(guid):
+        raise ValueError('article {!r} does not exist.'.format(guid))
+    cur.execute("DELETE FROM Article WHERE guid = %s", (guid,))
+
+
+def create_article(provider_id, category_id, url, guid, body, title,
+                  last_modified, author):
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO Article (provider_id, url, guid, body, title,
+                                last_modified, category_id, author)
+        SELECT %s, %s, %s, %s, %s, %s, %s, %s
+    """, (
+        provider_id,
+        url,
+        guid,
+        body,
+        title,
+        last_modified,
+        category_id,
+        author))
+    conn.commit()
