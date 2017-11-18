1fd83fbcbe3ff6c32108893b217a416049cd51ff
Lukas Stevens <lukas.steve@web.de>
2017-11-18 18:17:34 +0100

    Add sentence tokenization to synth

--- python-backend/nts_backend/synth.py
+++ python-backend/nts_backend/synth.py
@@ -7,6 +7,7 @@ import logging
 from . import database
 from .bingspeech import BingSpeechApi
 from .config import config
+from .tokenizer import Tokenizer, tokens_to_sentences
 
 
 def main():
@@ -14,4 +15,6 @@ def main():
     logging.basicConfig(level=logging.INFO)
     for article_id in database.get_articles(has_audioblob=False):
         article = database.get_article_by_id(article_id)
+        tok = Tokenizer(config['linguisticsapi']['key'])
+        sentences = tokens_to_sentences(tok.tokenize(article['summary'], 'en'))
         logging.info('SYNTHESIZING {} -- {}'.format(article_id, article['url']))
