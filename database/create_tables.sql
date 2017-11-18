fbe0306f1709a979ef039f41462d4f8a779caa61
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-11-18 11:28:06 +0100

    added keywords to db

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,7 +1,8 @@
-DROP TABLE Article;
-DROP TABLE Provider;
-DROP TABLE Category;
-
+DROP TABLE IF EXISTS ArticleToKeyword;
+DROP TABLE IF EXISTS Article;
+DROP TABLE IF EXISTS Keyword;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Provider;
 
 CREATE TABLE IF NOT EXISTS Provider (
   provider_id BIGSERIAL PRIMARY KEY,
@@ -13,10 +14,16 @@ CREATE TABLE IF NOT EXISTS Category (
   name        TEXT NOT NULL UNIQUE
 );
 
+CREATE TABLE IF NOT EXISTS Keyword (
+  keyword_id BIGSERIAL PRIMARY KEY,
+  name       TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Article (
   article_id      BIGSERIAL PRIMARY KEY,
   provider_id     BIGSERIAL REFERENCES Provider (provider_id),
   category_id     BIGSERIAL REFERENCES Category (category_id),
+  keyword_id      BIGSERIAL REFERENCES Keyword (keyword_id),
   guid            TEXT NOT NULL UNIQUE,
   url             TEXT NOT NULL,
   author          TEXT,
@@ -25,4 +32,10 @@ CREATE TABLE IF NOT EXISTS Article (
   summary         TEXT,
   last_modified   TIMESTAMP,
   last_summarized TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ArticleToKeyword (
+  article_id BIGSERIAL REFERENCES Article (article_id),
+  keyword_id BIGSERIAL REFERENCES Keyword (keyword_id),
+  PRIMARY KEY (article_id, keyword_id)
 );
