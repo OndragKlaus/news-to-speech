861ae15b6394d58645ec43cdbe563e3811587fd0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-18 11:48:57 +0100

    implement a API for article providers, + barebones implementation for Sueddeutsche Zeitung

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,51 +2,29 @@
 Scraper for news articles.
 """
 
-import argparse
-import asyncio
-from collections import namedtuple
-import feedparser
+
+from .provider import SueddeutscheZeitung
+
+import bs4
 import requests
-import sys
-import toml
 
-from database import conn
-from article import Article
+import database; database.init()
 
+providers = [
+    SueddeutscheZeitung()
+]
 
-async def get_rss_urls(rss_url):
-    loop = asyncio.get_event_loop()
-    response = await loop.run_in_executor(None, requests.get, rss_url)
-    feed = await loop.run_in_executor(None, feedparser.parse, response.text)
-    for item in feed['entries']:
-        yield item['link']
-
-
-async def fill_database():
-    sz_url = 'http://www.sueddeutsche.de/news/rss?search=Suchbegriff+eingeben&sort=date&all%5B%5D=dep&typ%5B%5D=article&sys%5B%5D=sz&sys%5B%5D=dpa&catsz%5B%5D=alles&catdpa%5B%5D=alles&time=P1D'
-    async for link in get_rss_urls(sz_url):
-        print(link)
-
-class Scraper:
-    ArticleTuple = namedtuple('ArticleTuple', ['article', 'source'])
-
-    def __init__():
-        self.articles = []
-        pass
-
-    def add_article(self, url, source=''):
-        article = Article(url=url)
-        self.articles.append(ArticleTuple(article, source))
-
-    def scrape(self):
-        for article, source in self.articles:
-            article.download()
-            article.parse(source)
-
-    def get_articles(self):
-        return [article_tuple.article for article_tuple in self.articles]
 
 def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fill_database())
-    loop.close()
+    database.init()
+    for provider in providers:
+        for info in provider.get_recent_article_urls():
+            print(info)
+            html = requests.get(info.url).text
+            soup = bs4.BeautifulSoup(html, 'lxml')
+            metadata = provider.get_article_metadata(info, html, soup)
+            summary = provider.summarize_article(info, html, soup)
+            print(' ', metadata)
+            print(' ', summary)
+            print('-' * 50)
+            # XXX Insert into database
---
+++ scraper/provider.py
@@ -0,0 +1,110 @@
+
+from typing import *
+from datetime import datetime
+
+import bs4
+import feedparser
+import requests
+import sumy.parsers.html
+import sumy.nlp.tokenizers
+import sumy.nlp.stemmers
+import sumy.summarizers.lsa
+import sumy.utils
+
+
+class ArticleUrl(NamedTuple):
+
+    id: str
+    url: str
+    language: str
+    data: Any = None
+
+
+class ArticleMetadata(NamedTuple):
+
+    authors: List[str] = None
+    category: str = None
+    keywords: List[str] = None
+    date_published: datetime = None
+
+
+class BaseProvider:
+
+    def get_recent_article_urls(self) -> Iterable[ArticleUrl]:
+        """
+        Returns a list of recent articles that need to be parsed. The method
+        may return articles that have been returned before, but they must be
+        identifiable by the globally unique #ArticleUrl.id.
+        """
+
+        raise NotImplementedError
+
+    def get_article_metadata(self, item: ArticleUrl, html: str,
+                             soup: bs4.BeautifulSoup) -> ArticleMetadata:
+        """
+        Extract additional metadata from the article at the URL *url*. You
+        get access to the pages' HTML content and the parsed BeautifulSoup
+        as well.
+        """
+
+        raise NotImplementedError
+
+    def summarize_article(self, item: ArticleUrl, html: str,
+                          soup: bs4.BeautifulSoup) -> str:
+        """
+        Summarize the article. The default implementation uses the #sumy
+        package.
+        """
+
+        SENTENCES_COUNT = 10  # XXX
+        tokenizer = sumy.nlp.tokenizers.Tokenizer(item.language)
+        parser = sumy.parsers.html.HtmlParser.from_string(html, item.url, tokenizer)
+        stemmer = sumy.nlp.stemmers.Stemmer(item.language)
+        summarizer = sumy.summarizers.lsa.LsaSummarizer(stemmer)
+        summarizer.stop_wors = sumy.utils.get_stop_words(item.language)
+        return ' '.join(map(str, summarizer(parser.document, SENTENCES_COUNT)))
+
+
+class RssProvider(BaseProvider):
+
+    language_map = {
+        'de': 'german',
+        'en': 'english',
+        'cz': 'czech',
+        'fr': 'frensh'
+        # XXX
+    }
+
+    def __init__(self, feed_url):
+        self.feed_url = feed_url
+
+    def get_recent_article_urls(self) -> Iterable[ArticleUrl]:
+        feed = feedparser.parse(requests.get(self.feed_url).text)
+        language = self.language_map[feed['feed']['language']]
+        for entry in feed['entries']:
+            yield ArticleUrl(entry['id'], entry['link'], language, data=entry)
+
+
+class SueddeutscheZeitung(RssProvider):
+
+    FEED_URL = 'http://www.sueddeutsche.de/news/rss?search=Suchbegriff+eingeben'\
+               '&sort=date&all%5B%5D=dep&typ%5B%5D=article&sys%5B%5D=sz&sys%5B%'\
+               '5D=dpa&catsz%5B%5D=alles&catdpa%5B%5D=alles&time=P1D'
+
+    def __init__(self):
+        super().__init__(self.FEED_URL)
+
+    def get_article_metadata(self, item: ArticleUrl, html: str,
+                             soup: bs4.BeautifulSoup) -> ArticleMetadata:
+        from .article import Article
+        import traceback
+
+        article = Article(item.url)
+        article.download(input_html=html)
+        try:
+            article.parse()
+        except:
+            traceback.print_exc()
+            return ArticleMetadata()
+
+        return ArticleMetadata(authors=article.authors)
