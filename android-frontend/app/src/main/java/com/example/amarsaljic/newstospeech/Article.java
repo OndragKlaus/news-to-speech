446115d13daf67e7a9ae066127be7bd8edaf62e4
Amar Šaljić <amarsaljic@w211-6k-v4.eduroam.dynamic.rbg.tum.de>
2017-11-18 19:29:36 +0100

    Created Article class

---
+++ android-frontend/app/src/main/java/com/example/amarsaljic/newstospeech/Article.java
@@ -0,0 +1,18 @@
+package com.example.amarsaljic.newstospeech;
+
+import java.util.Date;
+
+/**
+ * Created by amarsaljic on 18.11.17.
+ */
+
+public class Article {
+    String provider_name;
+    String category_name;
+    String url;
+    String author;
+    String title;
+    String summary;
+    Date date_published;
+    int audio_file_id;
+}
